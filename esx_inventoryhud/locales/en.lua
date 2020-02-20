24862df058269da8e4f8f2cc353f836774b5fae4
PulgaPT <61068298+PulgaPT@users.noreply.github.com>
2020-02-20 19:06:44 +0000

    Inventory

---
+++ esx_inventoryhud/locales/en.lua
@@ -0,0 +1,9 @@
+Locales['en'] = {
+	['cash'] = 'Cash',
+	['black_money'] = 'Dirty Money',
+	['player_nearby'] = 'No Players Nearby!',
+	['players_nearby'] = 'No Players Nearby!',
+	["player_info"] = "<strong>Name:</strong> %s<br><strong>Weight:</strong> %s / %s KG.",
+	["player_info_full"] = "<h4 style='color:red;'>Inventory Full</h4><br><strong>ชื่อ:</strong> %s<br><strong>Weight:</strong> %s / %s KG.",
+	["insufficient_space"] = "Inventory Full!"
+}
